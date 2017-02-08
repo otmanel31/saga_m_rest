56ee10a273a81b4ad0c2e810d95a1124f507143c
oelkenz <otman-82@hotmail.fr>
2017-02-08 16:47:08 +0100

    imple form up data

---
+++ routes/events.js
@@ -0,0 +1,80 @@
+const fs2 = require('fs')
+const express = require('express')
+const app = express()
+const path = require('path')
+const inspect = require('util').inspect
+const bodyParser = require('body-parser')
+const Event = require('./event_model')
+const formidable = require('formidable')
+const fs = require('fs-extra')
+
+app.use(bodyParser.json())
+
+module.exports = (db) => {
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname + '/event.html'))
+    })
+    app.post('/', (req, res) => {
+        // console.log(req)
+        console.log(inspect(req.body))
+        console.log('received ' + JSON.stringify(req.body))
+        let event = new Event()
+        let form = new formidable.IncomingForm()
+        form.parse(req, function(err, fields, files) {
+                console.log('data received\n\n')
+                console.log(inspect({ fields: fields, files: files }))
+                console.log('fields type ',inspect(fields.type))
+                event.type_event = fields.type
+                event.textarea = fields.text_event 
+           console.log('in files log ',files.file.name) 
+            })
+            /*0  event.type_event = req.body.type
+              event.textarea =  req.body.text_event*/
+
+        form.on('end', function(fields, files) {
+            var temp_path = this.openedFiles[0].path
+            var filename = this.openedFiles[0].path
+            var new_location = path.join(__dirname, '/upload')
+            fs.copy(temp_path, new_location + filename, function(err) {
+                if (err) console.log(err)
+                console.log('copy succes')
+                event.img.data = fs2.readFileSync(new_location + filename)
+                event.save(function(err, events) {
+                    console.log('in save')
+                    console.log(events)
+                    console.log('with inspect in save', inspect(events))
+                })
+            })
+            console.log(new_location+filename)
+            console.log(new_location)
+            console.log(filename)
+            var file = filename.split('/')
+            console.log('file name',file)
+        })
+       // event.img.data = req.body.data
+        //event.img.contentType = 'image/jpg'
+        /*event.save(function(err, events) {
+                console.log('in save')
+                if (err) console.log(err)
+                console.log(events)
+                console.log(inspect(events))
+                console.log('end')
+            })*/
+           // fs.unlink(new_location+'/tmp/'+file[2])
+            //res.setHeader('Location', 'samarche'))
+    })
+    app.get('/list', (req, res) => {
+        res.send('listes')
+    })
+    app.get('/:id', (req, res) => {
+        event = new Event()
+        console.log(req.params.id)
+        Event.findById(req.params.id, function(err, doc) {
+            if (err) return console.error(err)
+                //res.contentType(doc.img.contentType)
+                //let i = new Buffer(doc.img.data, 'binary')
+            res.send(doc.img.data)
+        })
+    })
+    return app
+}
--- server.js
+++ server.js
@@ -2,7 +2,22 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const alerts = require('./routes/alerts')
+const events = require('./routes/events')
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/saga')
+
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error'))
+db.on('open', function(){
+    console.log('connected on mongoDb')
+})
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
+
 app.use('/alerts', alerts())
+app.use('/events', events(db))
+
 
 const server = app.listen(3000, function() {
     var host = server.address().address
