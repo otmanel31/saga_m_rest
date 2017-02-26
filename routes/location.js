74cc91655f4849fa83a66d4d47881e7aa0d58571
Sylvain Cartier <asylvain10@cegetel.net>
2017-02-26 17:11:14 +0100

    add test for location route

--- routes/location.js
+++ routes/location.js
@@ -11,33 +11,41 @@ module.exports = (coorGPS) => {
     //######################################
 
     app.post('/:uuid_user', (req, res) => {
+        // test body is defined
+        if (req.body.Timestamp && req.body.Lattitude && req.body.Longitude) {
+            // create a new GPS coordinates
+            var GpsCoord = coorGPS({
+                uuid_user: req.params.uuid_user,
+                timestamp: req.body.Timestamp * 1000,
+                lattitude: req.body.Lattitude,
+                longitude: req.body.Longitude,
+                altitude: req.body.Altitude,
+                heading: req.body.Heading,
+                altitudeAccuracy: req.body.AltitudeAccuracy,
+                speed: req.body.Speed
+            });
 
-        // create a new GPS coordinates
-        var GpsCoord = coorGPS({
-            uuid_user: req.params.uuid_user,
-            timestamp: req.body.Timestamp,
-            lattitude: req.body.Lattitude,
-            longitude: req.body.Longitude,
-            altitude: req.body.Altitude,
-            heading: req.body.Heading,
-            altitudeAccuracy: req.body.AltitudeAccuracy,
-            speed: req.body.Speed
-        });
+            // Save a new GPS coordinates
+            GpsCoord.save(function(err, gpsCoordonnees) {
+                if (err) {
+                    throw err;
+                    res.status(500).end()
+                } else {
+                    //console.log(gpsCoordonnees)
+                    let uuid = gpsCoordonnees.uuid_user
+                    console.log('GPS coordinates created! uuid = ' + uuid);
+                    res.setHeader('Location', '/location/' + uuid);
+                    res.status(201).end()
+                }
 
-        // Save a new GPS coordinates
-        GpsCoord.save(function(err, gpsCoordonnees) {
-            if (err) {
-                throw err;
-                res.status(500).end()
-            } else {
-                //console.log(gpsCoordonnees)
-                let uuid = gpsCoordonnees.uuid_user
-                console.log('GPS coordinates created! uuid = ' + uuid);
-                res.setHeader('Location', '/location/' + uuid);
-                res.status(201).end()
-            }
+            });
 
-        });
+        } else {
+            // send error if body is undefined
+            res.status(400).end('Body contains not enough informations')
+
+        }
+
     })
 
     return app
---
+++ test/location.js
@@ -0,0 +1,149 @@
+//During the test the env variable is set to test
+process.env.NODE_ENV = 'test'
+
+let mongoose = require("mongoose")
+let coorGPS = require('../models').coorGPS
+
+//Require the dev-dependencies
+let chai = require('chai')
+let chaiHttp = require('chai-http')
+let server = require('../server')
+let setup = require('../setup_dev')
+let should = chai.should()
+
+
+chai.use(chaiHttp)
+chai.use(require('chai-datetime'));
+
+//Our parent block
+describe('location', () => {
+    var token = ''
+
+    before((done) => {
+
+        setup((err, msg) => {
+            if (err)
+                console.log('Authentication Test Error : ' + err)
+        })
+
+        //Before each test we get a authenticate token
+        let payload = {
+            name: 'Bar',
+            password: 'password'
+        }
+
+        //receive a token authenticate
+        chai.request(server)
+            .post('/authenticate')
+            .send(payload)
+            .end((err, res) => {
+                var result = JSON.parse(res.text);
+                token = result.token;
+                done()
+            })
+    })
+
+
+    beforeEach(() => {
+        //Before each test we empty the database
+        coorGPS.remove({}, (err) => {})
+    })
+
+    /*
+     * Test the /POST route
+     */
+    describe('/POST location', () => {
+
+        it('it should store coordinate in database', (done) => {
+
+            // load payload
+            let payload = {
+                Timestamp: 1340900279,
+                Lattitude: 48.862725,
+                Longitude: 2.287592,
+                Altitude: 1200,
+                AltitudeAccuracy: 0,
+                Heading: 45,
+                Speed: 100,
+            }
+
+            let uid_user = 123
+
+            // send POST request
+            chai.request(server)
+                .post('/location/' + uid_user)
+                .set('Authorization', 'Bearer ' + token)
+                .send(payload)
+                .end((err, res) => {
+                    res.should.have.status(201);
+                    coorGPS.findOne({ uuid_user: uid_user }, function(err, coorGPSFromDb) {
+                        let ConvertTimestamp = new Date(payload.Timestamp * 1000);
+                        (coorGPSFromDb.timestamp).should.equalTime(ConvertTimestamp);
+                        (coorGPSFromDb.lattitude).should.equal(payload.Lattitude);
+                        (coorGPSFromDb.longitude).should.equal(payload.Longitude);
+                        (coorGPSFromDb.altitude).should.equal(payload.Altitude);
+                        (coorGPSFromDb.altitudeAccuracy).should.equal(payload.AltitudeAccuracy);
+                        (coorGPSFromDb.heading).should.equal(payload.Heading);
+                        (coorGPSFromDb.speed).should.equal(payload.Speed);
+
+                        done()
+                    });
+                })
+        })
+
+        it('it should send Location header with a link to the id resource', (done) => {
+
+            // load payload
+            let payload = {
+                Timestamp: 1340900279,
+                Lattitude: 48.862725,
+                Longitude: 2.287592,
+                Altitude: 1200,
+                AltitudeAccuracy: 0,
+                Heading: 45,
+                Speed: 100,
+            }
+
+            let uid_user = 123
+
+            // send POST request
+            chai.request(server)
+                .post('/location/' + uid_user)
+                .set('Authorization', 'Bearer ' + token)
+                .send(payload)
+                .end((err, res) => {
+                    res.should.have.header('Location', '/location/' + uid_user)
+                    done()
+                });
+        })
+
+        it('it should returning an error if Timestamp, Lattitude, Longitude is not defined and not store in database', (done) => {
+
+            // load payload
+            let payload = {
+                Timestamp: null,
+                Lattitude: null,
+                Longitude: null,
+                Altitude: 1200,
+                AltitudeAccuracy: 0,
+                Heading: 45,
+                Speed: 100,
+            }
+
+            let uid_user = 456
+
+            // send POST request
+            chai.request(server)
+                .post('/location/' + uid_user)
+                .set('Authorization', 'Bearer ' + token)
+                .send(payload)
+                .end((err, res) => {
+
+                    coorGPS.findOne({ uuid_user: uid_user }, function(err, coorGPSFromDb) {
+                        should.not.exist(coorGPSFromDb)
+                        done();
+                    })
+                });
+        })
+    })
+})
